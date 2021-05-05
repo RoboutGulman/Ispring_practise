fd351e1d102c68c870b0b32258a664fa5edd9feb
RoboutGulman <79986929+RoboutGulman@users.noreply.github.com>
2021-05-05 15:21:48 +0300

    добавлены const и комментарии

--- src/Module/ImageProvider.php
+++ src/Module/ImageProvider.php
@@ -4,6 +4,7 @@ use IvanUskov\ImageSpider\ImageSpider;
 
 class ImageProvider implements ImageProviderInterface
 {
+    private const IMAGE_NUMBER = 5;
     private array $topic;
 
     public function getUrls(): void
@@ -14,9 +15,9 @@ class ImageProvider implements ImageProviderInterface
     public function getImageUrls(): array
     {
         $this->getUrls();
-        for ($i = 0; $i < 5; $i++)
+        for ($i = 0; $i < self::IMAGE_NUMBER; $i++)
         {
-            $this->urls = array_values(array_filter($this->urls));
+            $this->urls = array_values(array_filter($this->urls));//убирает элементы массива с пустым значением
             $lenght = count($this->urls);
             $index_image = mt_rand(0, ($lenght - 1));
             $images[] = $this->urls[$index_image];
@@ -28,12 +29,12 @@ class ImageProvider implements ImageProviderInterface
     public function addTopic($theme)
     {
         $this->topic[] = [
-            'header' => $this->theme = $theme,
+            'name' => $this->theme = $theme,
             'images' => $this->getImageUrls()
         ];
     }
 
-    public function  getData()
+    public function getData()
     {
         return $this->topic;
     }
