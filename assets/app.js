12729a52c4e699c016ae1d243478ad492a6d4988
RoboutGulman <79986929+RoboutGulman@users.noreply.github.com>
2021-06-04 12:41:37 +0300

    clean in app.js

--- assets/app.js
+++ assets/app.js
@@ -6,9 +6,6 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-//import './styles/app.css';
-import './modules/about_me/about_me.js';
 
-// start the Stimulus application
-//import './bootstrap';
-//require('./modules/about_me/about_me.js');
+import './modules/about_me/about_me.js';
+ 
